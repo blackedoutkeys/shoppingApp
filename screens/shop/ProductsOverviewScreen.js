270793895c9d32f8363ee1b1a529268426337b0d
Isaac Frishman <ifrishman89@gmail.com>
2021-04-09 09:41:57 -0500

    new screens, update components, and files.

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,10 +1,22 @@
-import React from 'react'
-import { FlatList }  from 'react-native'
+import React from 'react';
+import { FlatList, Text }  from 'react-native';
+import { useSelector } from 'react-redux';
+
+import ProductItem from '../../components/shop/ProductItem';
 
 const ProductOverviewScreen = props => {
-    return <FlatList />
+    const products = useSelector(state => state.products.availableProducts);
 
+    return( 
+    <FlatList 
+        data={products}
+        keyExtractor={item => item.id}
+        renderItem={itemData => <ProductItem />}
+    )
 }
 
+ProductOverviewScreen.navigationOptions = {
+    headerTitle: 'All Products'
+}
 
 export default ProductOverviewScreen
--- store/reducers/products.js
+++ store/reducers/products.js
@@ -1,6 +1,8 @@
- const initialState = {
-     availableProducts: [],
-     userProducts: []
+import PRODUCTS from '../../data/dummy-data';
+
+const initialState = {
+     availableProducts: PRODUCTS,
+     userProducts: PRODUCTS.filter(prod => prod.ownerId === 'u1')
  }
 
  export default (state = initialState, action) => {
