581a0dca89f842756353d421fabddde1ecfe5be2
Isaac <ifrishman89@gmail.com>
2021-04-09 23:17:49 -0500

    finalized project build

--- App.js
+++ App.js
@@ -1,16 +1,18 @@
 import React, { useState } from 'react';
 import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
-import AppLoading from 'expo-app-loading';
+import { AppLoading } from 'expo';
 import * as Font from 'expo-font';
 
 import productsReducer from './store/reducers/products';
 import cartReducer from './store/reducers/cart';
+import ordersReducer from './store/reducers/orders';
 import ShopNavigator from './navigation/ShopNavigator';
 
 const rootReducer = combineReducers({
   products: productsReducer,
-  cart: cartReducer
+  cart: cartReducer,
+  orders: ordersReducer
 });
 
 const store = createStore(rootReducer);
@@ -28,10 +30,10 @@ export default function App() {
   if (!fontLoaded) {
     return (
       <AppLoading
-      startAsync={fetchFonts}
-      onFinish={() => setFontLoaded(true)}
-      onError={console.warn}
-        
+        startAsync={fetchFonts}
+        onFinish={() => {
+          setFontLoaded(true);
+        }}
       />
     );
   }
