1b7113bd2d4a61042dd90ac3f56d019067aa682e
Isaac <ifrishman89@gmail.com>
2021-04-09 22:56:26 -0500

    update cart screen with styles, and Product Detail page fixed.

--- screens/shop/ProductDetailsScreen.js
+++ screens/shop/ProductDetailsScreen.js
@@ -1,70 +1,69 @@
 import React from 'react';
 import {
-  ScrollView,
-  View,
-  Text,
-  Image,
-  Button,
-  StyleSheet
+    View,
+    Text,
+    Image,
+    StyleSheet,
+    Button,
+    ScrollView
 } from 'react-native';
-import { useSelector, useDispatch } from 'react-redux';
 
+import {useSelector} from 'react-redux';
 import Colors from '../../constants/Colors';
-import * as cartActions from '../../store/actions/cart';
 
+//to have a valid component you need this to be jsx
 const ProductDetailScreen = props => {
-  const productId = props.navigation.getParam('productId');
-  const selectedProduct = useSelector(state =>
-    state.products.availableProducts.find(prod => prod.id === productId)
-  );
-  const dispatch = useDispatch();
+    const productId = props
+        .navigation
+        .getParam('productId');
+    // how to select single product by accessing state and the array you can find the
+    // item by its id
+    const selectedProduct = useSelector(state => state.products.availableProducts.find(prod => prod.id === productId));
 
-  return (
-    <ScrollView>
-      <Image style={styles.image} source={{ uri: selectedProduct.imageUrl }} />
-      <View style={styles.actions}>
-        <Button
-          color={Colors.primary}
-          title="Add to Cart"
-          onPress={() => {
-            dispatch(cartActions.addToCart(selectedProduct));
-          }}
-        />
-      </View>
-      <Text style={styles.price}>${selectedProduct.price.toFixed(2)}</Text>
-      <Text style={styles.description}>{selectedProduct.description}</Text>
-    </ScrollView>
-  );
+    return (
+        //flatlist is when page is infinitely long
+        <ScrollView> 
+            <Image style={styles.image} source={{uri: selectedProduct.imageUrl}} />
+            <View style={styles.actions}>
+                <Button color={Colors.primary} title="Add to Cart" onPress={() => {}} />
+            </View>
+            <Text style={styles.price} >${selectedProduct.price.toFixed(2)}</Text>
+            <Text style={styles.description}>{selectedProduct.description}</Text>
+        </ScrollView>
+    )
 };
 
 ProductDetailScreen.navigationOptions = navData => {
-  return {
-    headerTitle: navData.navigation.getParam('productTitle')
-  };
-};
+    return {
+        headerTitle: navData
+            .navigation
+            .getParam('productTitle')
+    }
+}
 
 const styles = StyleSheet.create({
-  image: {
-    width: '100%',
-    height: 300
-  },
-  actions: {
-    marginVertical: 10,
-    alignItems: 'center'
-  },
-  price: {
-    fontSize: 20,
-    color: '#888',
-    textAlign: 'center',
-    marginVertical: 20,
-    fontFamily: 'open-sans-bold'
-  },
-  description: {
-    fontFamily: 'open-sans',
-    fontSize: 14,
-    textAlign: 'center',
-    marginHorizontal: 20
-  }
-});
+    image: {
+        width: '100%',
+        height: 300,
+    },
+    price: {
+        fontSize: 20, 
+        color: '#888',
+        textAlign:'center',
+        marginVertical: 20,
+        fontFamily: 'open-sans-bold'
+    },
+    description: {
+        fontSize: 14,
+        textAlign: 'center',
+        marginHorizontal: 20,
+        fontFamily: 'open-sans'
+    },
+    actions: {
+        marginVertical: 10,
+        alignItems: 'center'
+    }
 
-export default ProductDetailScreen;
+})
+
+export default ProductDetailScreen
